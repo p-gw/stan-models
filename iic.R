c831b9e19726c64d628eccd740a4177dc17bb0d7
Philipp Gewessler <p.gewessler@gmx.at>
2017-07-23 16:17:03 +0200

    added item information plot

--- iic.R
+++ iic.R
@@ -1,8 +1,8 @@
 #' @title
-#' item characteristic curve
+#' item information curve
 #' 
 #' @description
-#' plots the item characteristic curve for a given item
+#' plots the item information for a given item
 #' 
 #' @param   item          item indicator (an integer)
 #' @param   data          posterior samples from an IRT model
@@ -11,19 +11,19 @@
 #'
 #' @export
 #'
-icc <- function(item, data, range = 5, random_draws = 0, seed = NULL) {
+iic <- function(item, data, range = 5, random_draws = 0, seed = NULL) {
   if (!is.null(seed)) {
     set.seed(seed)
   }
 
   d <- data.table("x" = seq(-range, range, length.out = 200))
-  d[, "y" := icf(alpha = mean(data$alpha[,item]), beta = mean(data$beta[,item]), x)]
+  d[, "y" := iif(alpha = mean(data$alpha[,item]), beta = mean(data$beta[,item]), x)]
 
   if (random_draws > 0) { 
     s <- sample(1:nrow(data$beta), random_draws, replace = FALSE)
 
     for (i in 1:random_draws) {
-      d[, paste0("v", i) := icf(
+      d[, paste0("v", i) := iif(
         alpha = mean(data$alpha[s[i], item]), 
         beta = mean(data$beta[s[i], item]), 
         x)
@@ -36,20 +36,19 @@ icc <- function(item, data, range = 5, random_draws = 0, seed = NULL) {
 
   # init plot
   ggplot(d, aes(x, value, group = variable)) + 
-    geom_path(colour = "#80CBC4") +
+    geom_path(colour = "#CE93D8") +
     geom_path(aes(colour = ind), size = 1) + 
-    scale_colour_manual(values = c("#009688", "transparent"), guide = "none") + 
+    scale_colour_manual(values = c("#9C27B0", "transparent"), guide = "none") + 
     scale_x_continuous(limits = c(-range, range), breaks = c(-range, 0, range), expand = c(0, 0)) + 
-    scale_y_continuous(limits = c(0, 1), breaks = c(0, 0.5, 1), expand = c(0, 0.05)) + 
     xlab(expression(theta)) + 
-    ylab("P(Y = 1)")
+    ylab("information")
 }
 
 #' @title
-#' item characteristic function
+#' item information function
 #' 
 #' @description 
-#' calculates the value of the item characteristic function
+#' calculates the value of the item information function
 #'
 #' @param   alpha   item discrimination
 #' @param   beta    item difficulty
@@ -57,6 +56,6 @@ icc <- function(item, data, range = 5, random_draws = 0, seed = NULL) {
 #'
 #' @export
 #'
-icf <- function(alpha, beta, theta) {
-  (1 + exp(-alpha*(theta - beta)))^-1
+iif <- function(alpha, beta, theta) {
+  alpha^2 * icf(alpha, beta, theta) * (1 - icf(alpha, beta, theta))
 }
